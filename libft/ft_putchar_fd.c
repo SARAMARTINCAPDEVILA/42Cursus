14accc1712f95710f75a021ab5d7879c1d0a466e
Sara Martin <smartin@cbr12s3.42barcelona.com>
2022-10-07 18:15:23 +0200

    07-10-2022bien

--- libft/ft_putchar_fd.c
+++ libft/ft_putchar_fd.c
@@ -6,23 +6,13 @@
 /*   By: smartin <smartin@student.42barcelo>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 18:56:33 by smartin           #+#    #+#             */
-/*   Updated: 2022/09/30 17:41:14 by smartin          ###   ########.fr       */
+/*   Updated: 2022/09/28 19:06:22 by smartin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <unistd.h>
-#include <fcntl.h>
+
 #include "libft.h"
 
 void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
-}
-
-int	main(void)
-{
-	int	fd;
-	char c = 'a';
-	fd = open("test.txt", O_RDWR);
-	ft_putchar_fd(c, fd);
-	return (0);
 }
